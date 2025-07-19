ce161fee4f03e755126556935bf994fac670878d
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-07-19 22:04:16 +0530

    Create 1752. CheckifArrayIsSortedandRotated.cpp

---
+++ 1752. CheckifArrayIsSortedandRotated/1752. CheckifArrayIsSortedandRotated.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool check(vector<int>& arr) {
+        int count=0;
+        for(int i =0;i<arr.size();i++){
+            if(arr[i]>arr[(i+1)%arr.size()]){
+                count++;
+            }
+        }
+        if(count > 1){
+            return false;
+        }else{
+            return true;
+        }
+        
+    }
+};
