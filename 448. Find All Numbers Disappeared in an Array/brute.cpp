3557f9b29b89bf8c0be96f355782d59b0cbade02
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-08-04 08:28:24 +0530

    Update brute.cpp

--- 448. Find All Numbers Disappeared in an Array/brute.cpp
+++ 448. Find All Numbers Disappeared in an Array/brute.cpp
@@ -6,8 +6,8 @@ public:
         for(int i =0;i<nums.size();i++){
             v1[nums[i]]++;
         }
-        for(int i =0;i<v1.size();i++){
-            if(v1[i]==0 && i!=0){
+        for(int i =1;i<v1.size();i++){
+            if(v1[i]==0){
                 sol.push_back(i);
             }
         }
