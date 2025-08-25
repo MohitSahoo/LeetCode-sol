313f931d77d48275caf3f8947286cbbfac073da3
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-08-25 12:09:16 +0530

    Time: 8 ms (11.71%) | Memory: 19.7 MB (29.93%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -10,8 +10,6 @@ public:
         for (int i = 1; i <= n; i++) {
           
             dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i-1]);
-
-            // holding stock today:
             dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i-1]);
         }
 
