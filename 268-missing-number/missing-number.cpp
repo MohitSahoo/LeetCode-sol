2825acd08f4bd59975b0f58cf89cc9df01ef0e40
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-08-28 19:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.16%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -4,10 +4,10 @@ public:
         int xor1=0;
         int xor2=0;
         for(int i =0;i<nums.size();i++){
-            xor1=xor1^i;
+            xor1=xor1^(i+1);
             xor2=xor2^nums[i];
         }
-        xor1=xor1^(nums.size());
+        
         return xor1^xor2;
         
     }
