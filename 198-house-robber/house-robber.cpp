464a3c96f3b3979682c0dee518b7e7599c102e83
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-09-01 09:08:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.85%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int dp[401];
-    int solve(vector<int>& nums, int n){
-        if (n < 0) return 0;
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        int notake = 0+solve(nums,n-1);
-        int take = nums[n]+solve(nums,n-2);
-      
-        return dp[n]= max(take,notake);
-    }
     int rob(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        int n = nums.size()-1;
-        return solve(nums,n);
-        
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i =2;i<nums.size();i++){
+            int notake = 0+ dp[i-1];
+            int take = nums[i]+dp[i-2];
+            dp[i]=max(take,notake);
+        }
+        return dp[n-1];   
     }
 };
