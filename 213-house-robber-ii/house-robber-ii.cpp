fd2a8c5168dc01af007472723952f58a80ae9481
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-09-01 10:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (40.79%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int robLinear(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
         if (n == 1) return nums[0];
 
         vector<int> dp(n, 0);
