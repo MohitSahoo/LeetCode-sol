2e902be196251996052d1bb6799c4ee403d9962d
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-09-02 21:39:30 +0530

    Time: 9 ms (8.21%) | Memory: 9.8 MB (48.46%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        map<char,int>mpp;
+        for(int i =0;i<s.size();i++){
+            mpp[s[i]-'a']++;
+        }
+        for(int i =0;i<t.size();i++){
+            mpp[t[i]-'a']--;
+            if(mpp[t[i]-'a']==0){
+                mpp.erase(t[i]-'a');
+            }
+        }
+        if(mpp.empty()){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
