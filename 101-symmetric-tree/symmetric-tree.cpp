8bd0f4de93d647174d314a8405dd98b7931cd9d0
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-09-08 21:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (97.95%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -18,9 +18,7 @@ public:
         if(left == NULL || right == NULL){
             return left == right;
         }
-        if(left->val != right->val){
-            return false;
-        }
-        return isSymmetricHelp(left->left,right->right)&&isSymmetricHelp(left->right,right->left);
+       
+        return (left->val == right->val) && isSymmetricHelp(left->left,right->right)&&isSymmetricHelp(left->right,right->left);
     }
 };
