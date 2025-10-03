daf70d23240f4200b8b96083ac9461c269d0e8b7
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-10-03 19:28:01 +0530

    Time: 3 ms (72.57%) | Memory: 18.3 MB (59.31%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<int>& visited, vector<int>& pathVisited, vector<int>& order) {
-        visited[node] = 1;
-        pathVisited[node] = 1; // mark current node in recursion stack
-
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                if (!dfs(neighbor, adj, visited, pathVisited, order)) return false;
-            } else if (pathVisited[neighbor]) {
-                // cycle detected
-                return false;
-            }
-        }
-
-        pathVisited[node] = 0; // backtrack
-        order.push_back(node);  // post-order
-        return true;
-    }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
         for (auto& pre : prerequisites) {
             adj[pre[1]].push_back(pre[0]);
         }
-
-        vector<int> visited(numCourses, 0);
-        vector<int> pathVisited(numCourses, 0);
-        vector<int> order;
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (!dfs(i, adj, visited, pathVisited, order)) return {}; // cycle detected
+        vector<int>indegree(numCourses,0);
+        for(int i =0;i<numCourses;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
         }
+        queue<int>q;
+        for(int i =0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
 
-        reverse(order.begin(), order.end());
-        return order;
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(topo.size()==numCourses){
+            return topo;
+        }else{
+            return {};
+        }
+        
     }
 };
