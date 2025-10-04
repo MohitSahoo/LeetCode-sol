1bf0a557116dd24543cd1afd837168dbda02bb2e
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-10-04 14:27:37 +0530

    Time: 58 ms (62.68%) | Memory: 21.3 MB (46.12%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(beginWord == endWord){
+            return 0;
+        }
         queue<pair<string,int>>q;
         q.push({beginWord,1});
         unordered_set<string>st(wordList.begin(),wordList.end());
