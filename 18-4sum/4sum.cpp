43f00910cf7723c3c1728e9b1d1a5e527434b573
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-10-26 15:51:44 +0530

    Time: 22 ms (56.74%) | Memory: 17.5 MB (55.31%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) { 
-        int n = nums.size();
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        for (int i =0;i<nums.size();i++){
-            if( i>0 && nums[i]== nums[i-1]){
-                continue;
-            }
-            for(int j =i+1;j<n;j++){
-                if(j>i+1 && nums[j] == nums[j-1]){
-                    continue;
-                }
-                int k = j+1;
-                int l = n-1;
-                while(k<l){
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate for i
+
+            for (int j = i + 1; j < nums.size() ; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip duplicate for j
+
+                int k = j + 1, l = nums.size() - 1;
+
+                while (k < l) {
                     long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
-                    if(sum == target){
-                        vector<int>temp ={nums[i],nums[j],nums[k],nums[l]};
-                        ans.push_back(temp);
+                    if (sum < target) {
                         k++;
+                    } else if (sum > target) {
                         l--;
-                        while(k<l && nums[k]==nums[k-1]){
-                            k++;
-                        }
-                        while(k<l && nums[l]==nums[l+1]){
-                            l--;
-                        }
+                    } else {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
 
-                    }else if(sum >target){
-                        l--;
-                    }else{
                         k++;
+                        l--;
+
+                        // Skip duplicates for both k and l
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
                     }
                 }
             }
-        }  
+        }
+
         return ans;
     }
-
 };
