5d7df67e1b2f60735d1d7c0935893d7eb2d5bcea
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-10-26 11:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (80.39%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,18 +4,16 @@ public:
         if(nums.size()==1){
             return nums[0];
         }
-        int sum =0;
-        int maxsum =nums[0];
+        int sum = 0;
+        int maxsum = nums[0];
         for(int i =0;i<nums.size();i++){
-            sum += nums[i];
-            if(sum > maxsum){
-                maxsum = max(maxsum,sum);
+            sum+=nums[i];
+            if(sum >maxsum){
+                maxsum = sum;
             }
-            if(sum < 0){
-                sum =0;
+            if(sum <0){
+                sum = 0;
             }
-
-            
         }
         return maxsum;
         
