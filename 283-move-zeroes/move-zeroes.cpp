cf18ef104b6f26effcc664b3c7592fdc2ebfc1ce
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-10-28 20:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,22 +4,17 @@ public:
         int j =0;
         for(int i =0;i<nums.size();i++){
             if(nums[i]==0){
-                j=i;
+                j =i;
                 break;
             }
         }
         int k = j+1;
         while(k<nums.size()){
-            if(nums[j]==0 && nums[k]!=0){
-                swap(nums[j],nums[k]);
+            if(nums[k]!=0 && nums[j]==0){
+                swap(nums[k],nums[j]);
                 j++;
-                k++;
-            }else{
-                k++;
             }
-
+            k++;
         }
-        
-        
     }
 };
