78f0dbee26d85557b44a786a0d75366e3b4936c8
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2025-12-19 23:53:50 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (89.05%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -7,19 +7,15 @@ public:
         if(nums.size() == 1){
             return nums[0];
         }
-        vector<int>dp(nums.size(),-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        int take =0;
-        int nottake =0;
+        int prev2 = nums[0];
+        int prev1 = max(nums[0],nums[1]);
         for(int i =2;i<nums.size();i++){
-            take=nums[i]+dp[i-2];
-            nottake = 0 + dp[i-1];
-            dp[i]=max(take,nottake);
-
+            int take = prev2 + nums[i];
+            int no = 0+ prev1;
+            int cur= max(take,no);
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return dp[nums.size()-1];
-
-        
+        return prev1;
     }
 };
