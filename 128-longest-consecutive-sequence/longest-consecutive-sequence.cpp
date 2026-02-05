87992f71e35c4afbf263e3d8a53167785fdfae2f
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2026-02-05 21:42:04 +0530

    Time: 81 ms (57.07%) | Memory: 88.8 MB (63.50%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -10,10 +10,9 @@ public:
         unordered_set<int>s1(nums.begin(),nums.end());
         int maxcount = -1;
         for(auto it:s1){
-            int count =0;
+            int count =1;
             if(s1.find(it-1)==s1.end()){
                 int x = it;
-                count++;
                 while(s1.find(x+1)!=s1.end()){
                     count++;
                     x++;
