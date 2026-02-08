7202f22e2369d3dd74339b1ba8709b27dc90ade0
Mohit Kumar Sahoo <150445335+MohitSahoo@users.noreply.github.com>
2026-02-09 00:10:43 +0530

    Time: 14 ms (96.08%) | Memory: 70.6 MB (12.39%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,34 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int func(vector<int>&bloomDay,int mid,int k){
-        int total=0;
-        int count =0;
-        for(int i =0;i<bloomDay.size();i++){
+    // Helper function to calculate the number of bouquets that can be made
+    int nob(vector<int>& bloomDay, int k, int mid) {
+        int count = 0;   // Count the number of flowers in a bouquet
+        int bouquets = 0; // Total bouquets formed
+        for (int i = 0; i < bloomDay.size(); i++) {
             if(bloomDay[i]<=mid){
                 count++;
+                if(count ==k){
+                    bouquets++;
+                    count=0;
+                }
             }else{
-                total += count / k;
-                count = 0;
-            }  
+                count =0;
+            }
+            
         }
-        total += count / k;
-        return total;
+        return bouquets;
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        int low = *std::min_element(bloomDay.begin(),bloomDay.end());
-        int high = *std::max_element(bloomDay.begin(),bloomDay.end());
-        int ans =-1;
 
-        while(low <= high){
-            int mid =  (low + high)/2;
-            if(func(bloomDay,mid,k)>=m){
-                ans = mid;
-                high = mid-1;
-            }else{
-                low = mid+1;
+    // Function to find the minimum day to make 'm' bouquets of 'k' flowers
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int low = 1;
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+        int ans = -1; // Initialize the answer to -1, in case no solution is found
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int bouquets = nob(bloomDay, k, mid); // Get the number of bouquets for the current day 'mid'
+
+            if (bouquets >= m) {
+                ans = mid;  // If we can form 'm' or more bouquets, try for a smaller day
+                high = mid - 1;
+            } else {
+                low = mid + 1;  // If not enough bouquets, try a larger day
             }
         }
-        return ans;
-        
+
+        return ans; // Return the minimum day if a solution exists, otherwise -1
     }
 };
+
+
